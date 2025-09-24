9d79a936f51321fe4156945185ae3609ad62672f
brunolzrosa <brunoluiszrosa@gmail.com>
2025-09-24 04:08:50 -0300

    alterei o arquivo gerador de datas

--- sql_scripts/DML_DW_insert_dates.sql
+++ sql_scripts/DML_DW_insert_dates.sql
@@ -2,25 +2,25 @@
 -- ETL: Carga da Dimensão de Tempo
 -- ===================================================================
 -- Script que insere todos os dias de 1 de janeiro de 2020 a 31 de dezembro de 2030
--- na tabela Dim_Tempo, já calculando todos os atributos necessários
+-- na tabela DimCalendario, já calculando todos os atributos necessários
 -- como dia da semana, mês, trimestre e ano.
 
-INSERT INTO dw.Dim_Tempo (
-    PK_Tempo,
-    Data_Completa,
+INSERT INTO dw.DimCalendario (
+    KeyCalendario,
+    DataCompleta,
     Ano,
     Trimestre,
     Mes,
-    Nome_Mes,
-    Dia_do_Mes,
-    Dia_Semana
+    NomeMes,
+    DiaDoMes,
+    DiaDaSemana
 )
 SELECT
     -- Cria uma chave primária numérica no formato YYYYMMDD 
-    TO_CHAR(dt, 'YYYYMMDD')::INT AS PK_Tempo,
+    TO_CHAR(dt, 'YYYYMMDD')::INT AS KeyCalendario,
     
     -- A data completa
-    dt AS Data_Completa,
+    dt AS DataCompleta,
     
     -- Extrai o ANO da data
     EXTRACT(YEAR FROM dt)::INT AS Ano,
@@ -32,18 +32,18 @@ SELECT
     EXTRACT(MONTH FROM dt)::INT AS Mes,
     
     -- Extrai o nome do mês 
-    TO_CHAR(dt, 'TMMonth') AS Nome_Mes,
+    TO_CHAR(dt, 'TMMonth') AS NomeMes,
     
     -- Extrai o DIA do mês
-    EXTRACT(DAY FROM dt)::INT AS Dia_do_Mes,
+    EXTRACT(DAY FROM dt)::INT AS DiaDoMes,
 
     -- Extrai o nome do dia da semana 
-    TO_CHAR(dt, 'TMDay') AS Dia_Semana
+    TO_CHAR(dt, 'TMDay') AS DiaDaSemana
 
 FROM
     -- Gera uma série de datas, do início ao fim, com intervalo de 1 dia
     generate_series(
-        '2020-01-01'::DATE,
-        '2030-12-31'::DATE,
+        '2010-01-01'::DATE,
+        '2025-09-24'::DATE,
         '1 day'::INTERVAL
     ) AS s(dt); --alias
